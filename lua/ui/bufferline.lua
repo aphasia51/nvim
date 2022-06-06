ed175523e11cfdaf0aaf2df3284d08a187106ce4
aphasia51 <xiahuas@outlook.com>
2022-06-06 22:44:37 +0800

    update

--- lua/ui/bufferline.lua
+++ lua/ui/bufferline.lua
@@ -1,20 +1,29 @@
 require("bufferline").setup({
   options = {
     offsets = { { filetype = "NvimTree", text = " Explorer", padding = 1 } },
+    indicator_icon = "▎",
     buffer_close_icon = "",
     modified_icon = "",
     close_icon = "",
     left_trunc_marker = "",
     right_trunc_marker = "",
-    max_name_length = 14,
-    max_prefix_length = 13,
-    tab_size = 20,
-    diagnostic = false,
+    max_name_length = 18,
+    max_prefix_length = 18,
+    tab_size = 18,
     show_tab_indicators = true,
     enforce_regular_tabs = false,
-    view = "multiwindow",
     show_buffer_close_icons = true,
     separator_style = "thin",
     always_show_bufferline = true,
   },
+  highlights = {
+		fill = {
+			guifg = { attribute = "fg", highlight = "#ff0000" },
+			guibg = { attribute = "bg", highlight = "TabLine" },
+		},
+		background = {
+			guifg = { attribute = "fg", highlight = "TabLine" },
+			guibg = { attribute = "bg", highlight = "TabLine" },
+		},
+  }
 })

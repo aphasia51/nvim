514209ab6a66b26e0a651aab77cf7e01104bcc7a
aphasia51 <xiahuas@outlook.com>
2022-06-28 15:06:20 +0800

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -111,6 +111,7 @@ vim.g.loaded_python_provider = 0
 
 vim.g.vsnip_snippet_dir = vim.fn.expand("~/.config/nvim/vsnip")
 
+--disable_distribution_plugins
 vim.g.loaded_gzip              = 1
 vim.g.loaded_tar               = 1
 vim.g.loaded_tarPlugin         = 1
@@ -121,13 +122,13 @@ vim.g.loaded_getscriptPlugin   = 1
 vim.g.loaded_vimball           = 1
 vim.g.loaded_vimballPlugin     = 1
 vim.g.loaded_matchit           = 1
-vim.g.loaded_netrwPlugin       = 0
-vim.g.loaded_matchparen        = 0
-vim.g.loaded_2html_plugin      = 0
-vim.g.loaded_logiPat           = 0
-vim.g.loaded_rrhelper          = 0
-vim.g.loaded_netrw             = 0
-vim.g.loaded_netrwSettings     = 0
-vim.g.did_load_filetypes       = 0
-vim.g.do_filetype_lua          = 1
-vim.g.loaded_netrwFileHandlers = 0
+vim.g.loaded_matchparen        = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_logiPat           = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.did_load_filetypes = 0
+vim.g.do_filetype_lua = 1

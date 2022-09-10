8624762643730c91da7d37bcca70afae82d15171
aphasia51 <xiahsong51@gmail.com>
2022-09-10 23:56:51 +0800

    fix codeAction error

--- lua/internal/autocmds.lua
+++ lua/internal/autocmds.lua
@@ -75,6 +75,29 @@ api.nvim_create_autocmd("Filetype", {
 	end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = my_group,
+  pattern = '*.go',
+  callback = function()
+    if not packer_plugins['lspconfig'] then
+      return
+    end
+    local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+    params.context = { only = { 'source.organizeImports' } }
+
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end,
+})
+
 -- 反卷小助手
 local work_time = 1000 * 60 * 20
 vim.api.nvim_create_autocmd({ "BufEnter" }, {

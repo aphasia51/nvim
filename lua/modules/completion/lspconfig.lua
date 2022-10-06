18c1f91e05218906ddee2aa92875a89077d5f9e2
aphasia51 <xiahsong51@gmail.com>
2022-10-06 20:13:11 +0800

    update

--- lua/modules/completion/lspconfig.lua
+++ lua/modules/completion/lspconfig.lua
@@ -4,130 +4,130 @@ local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 if not packer_plugins["cmp-nvim-lsp"].loaded then
-	vim.cmd([[packadd cmp-nvim-lsp]])
+  vim.cmd([[packadd cmp-nvim-lsp]])
 end
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local signs = {
-	Error = " ",
-	Warn = " ",
-	Info = " ",
-	Hint = " ",
+  Error = " ",
+  Warn = " ",
+  Info = " ",
+  Hint = " ",
 }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config({
-	signs = true,
-	update_in_insert = false,
-	underline = true,
-	severity_sort = true,
-	virtual_text = {
-		prefix = "﮿",
-		spacing = 4,
-		source = true,
-		severity = { min = vim.diagnostic.severity.WARN },
-	},
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  virtual_text = {
+    prefix = "﮿",
+    spacing = 4,
+    source = true,
+    severity = { min = vim.diagnostic.severity.WARN },
+  },
 })
 
 local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentFormattingProvider then
-		api.nvim_create_autocmd("BufWritePre", {
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({
-					bufnr = bufnr,
-					async = true,
-				})
-			end,
-		})
-	end
+  if client.server_capabilities.documentFormattingProvider then
+    api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({
+          bufnr = bufnr,
+          async = true,
+        })
+      end,
+    })
+  end
 end
 
 lspconfig.gopls.setup({
-	on_attach = on_attach,
-	cmd = { "gopls", "--remote=auto" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
-	capabilities = capabilities,
-	init_options = {
-		usePlaceholders = true,
-		completeUnimported = true,
-	},
+  on_attach = on_attach,
+  cmd = { "gopls", "--remote=auto" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
+  capabilities = capabilities,
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+  },
 })
 
 lspconfig.sumneko_lua.setup({
-	on_attach = on_attach,
-	filetypes = { "lua" },
-	root_dir = lspconfig.util.root_pattern(
-		".luarc.json",
-		".luacheckrc",
-		".stylua.toml",
-		"stylua.toml",
-		"selene.toml",
-		".git"
-	),
-	settings = {
-		Lua = {
-			diagnostics = {
-				enable = true,
-				globals = { "vim", "packer_plugins" },
-			},
-			runtime = { version = "LuaJIT" },
-			workspace = {
-				library = vim.list_extend({ [vim.fn.expand("$VIMRUNTIME/lua")] = true }, {}),
-			},
-		},
-	},
+  on_attach = on_attach,
+  filetypes = { "lua" },
+  root_dir = lspconfig.util.root_pattern(
+    ".luarc.json",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    ".git"
+  ),
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = true,
+        globals = { "vim", "packer_plugins" },
+      },
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        library = vim.list_extend({ [vim.fn.expand("$VIMRUNTIME/lua")] = true }, {}),
+      },
+    },
+  },
 })
 
---lspconfig.clangd.setup({
---	on_attach = on_attach,
---	cmd = {
---		"clangd",
---		"--background-index",
---		"--suggest-missing-includes",
---		"--clang-tidy",
---		"--header-insertion=iwyu",
---	},
---	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
---	root_dir = lspconfig.util.root_pattern(
---		".clangd",
---		".clang-tidy",
---		".clang-format",
---		"compile_commands.json",
---		"compile_flags.txt",
---		"configure.ac",
---		".git"
---	),
---})
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--suggest-missing-includes",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  root_dir = lspconfig.util.root_pattern(
+    ".clangd",
+    ".clang-tidy",
+    ".clang-format",
+    "compile_commands.json",
+    "compile_flags.txt",
+    "configure.ac",
+    ".git"
+  ),
+})
 
 lspconfig.pyright.setup({
-	on_attach = on_attach,
-	cmd = { "pyright-langserver", "--stdio" },
-	filetypes = { "python" },
-	root_dir = lspconfig.util.root_pattern(unpack({
-		"pyproject.toml",
-		"setup.py",
-		"setup.cfg",
-		"requirements.txt",
-		"Pipfile",
-		"pyrightconfig.json",
-		-- customize
-		"manage.py",
-		"run.py",
-		".git",
-	})),
+  on_attach = on_attach,
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_dir = lspconfig.util.root_pattern(unpack({
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+    "pyrightconfig.json",
+    -- customize
+    "manage.py",
+    "run.py",
+    ".git",
+  })),
 })
 
 local servers = {
-	"bashls",
+  "bashls",
 }
 
 for _, server in ipairs(servers) do
-	lspconfig[server].setup({
-		on_attach = on_attach,
-	})
+  lspconfig[server].setup({
+    on_attach = on_attach,
+  })
 end

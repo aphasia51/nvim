5ceca0d43be4aebd039f0ede98ebfb725f3682f0
aphasia51 <xiahuas@outlook.com>
2022-10-15 16:30:46 +0800

    update

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -1,25 +1,25 @@
 local package = require("core.pack").package
 local conf = require("modules.ui.config")
 
-package({ "~/software/zephyr-nvim", config = conf.zephyr })
+package({ "aphasia51/zephyr-nvim", config = conf.zephyr })
 
 package({
-	"glepnir/galaxyline.nvim",
-	config = conf.galaxyline,
-	event = { "BufReadPre", "BufNewFile" },
-	requires = "kyazdani42/nvim-web-devicons",
+  "glepnir/galaxyline.nvim",
+  config = conf.galaxyline,
+  event = { "BufReadPre", "BufNewFile" },
+  requires = "kyazdani42/nvim-web-devicons",
 })
 
 package({
-	"lukas-reineke/indent-blankline.nvim",
-	event = "BufRead",
-	config = conf.indent_blankline,
+  "lukas-reineke/indent-blankline.nvim",
+  event = "BufRead",
+  config = conf.indent_blankline,
 })
 
 package({
-	"akinsho/bufferline.nvim",
-	disable = true,
-	requires = "kyazdani42/nvim-web-devicons",
-	config = conf.bufferline,
-	event = { "BufRead", "BufNewFile" },
+  "akinsho/bufferline.nvim",
+  disable = true,
+  requires = "kyazdani42/nvim-web-devicons",
+  config = conf.bufferline,
+  event = { "BufRead", "BufNewFile" },
 })

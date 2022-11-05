293600ad10d88516b973278f03706ea6c59478d7
aphasia51 <xiahuas@outlook.com>
2022-11-05 23:03:07 +0800

    formatter

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -1,53 +1,53 @@
 local config = {}
 
 function config.zephyr()
-	vim.cmd("colorscheme zephyr")
+  vim.cmd("colorscheme zephyr")
 end
 
 function config.galaxyline()
-	require("modules.ui.statusline")
+  require("modules.ui.statusline")
 end
 
 function config.indent_blankline()
-	vim.opt.list = true
-	vim.opt.listchars:append("space: ")
-	require("indent_blankline").setup({
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = true,
-		filetype_exclude = {
-			"dashboard",
-			"DogicPrompt",
-			"log",
-			"fugitive",
-			"gitcommit",
-			"packer",
-			"markdown",
-			"json",
-			"txt",
-			"vista",
-			"help",
-			"todoist",
-			"NvimTree",
-			"git",
-			"TelescopePrompt",
-			"undotree",
-		},
-		buftype_exclude = { "terminal", "nofile", "prompt" },
-		context_patterns = {
-			"class",
-			"function",
-			"method",
-			"block",
-			"list_literal",
-			"selector",
-			"^if",
-			"^table",
-			"if_statement",
-			"while",
-			"for",
-		},
-	})
+  vim.opt.list = true
+  vim.opt.listchars:append("space: ")
+  require("indent_blankline").setup({
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    filetype_exclude = {
+      "dashboard",
+      "DogicPrompt",
+      "log",
+      "fugitive",
+      "gitcommit",
+      "packer",
+      "markdown",
+      "json",
+      "txt",
+      "vista",
+      "help",
+      "todoist",
+      "NvimTree",
+      "git",
+      "TelescopePrompt",
+      "undotree",
+    },
+    buftype_exclude = { "terminal", "nofile", "prompt" },
+    context_patterns = {
+      "class",
+      "function",
+      "method",
+      "block",
+      "list_literal",
+      "selector",
+      "^if",
+      "^table",
+      "if_statement",
+      "while",
+      "for",
+    },
+  })
 end
 
 return config

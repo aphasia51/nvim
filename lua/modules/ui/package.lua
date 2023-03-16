2b0f5dd08b780c117a3a8ee5554e61a4f608bd35
aphasia51 <xiahuas@outlook.com>
2023-03-16 17:03:17 +0800

    clean some codes

--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -9,23 +9,10 @@ package({
 
 package({
   'glepnir/galaxyline.nvim',
-  -- enabled = false,
   config = conf.galaxyline,
   event = { 'BufRead', 'BufNewFile' },
-  -- event = { 'VeryLazy' },
   dependencies = 'nvim-tree/nvim-web-devicons',
   lazy = true,
-})
-
-package({
-  'glepnir/whiskyline.nvim',
-  enabled = false,
-  dev = true,
-  event = { 'VeryLazy' },
-  config = function()
-    require('whiskyline').setup()
-  end,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
 })
 
 package({

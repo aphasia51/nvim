3559e05f57febdc946597cfe2ec147d26c5f374a
aphasia51 <xiahuas@outlook.com>
2023-03-23 12:44:10 +0800

    update statuscolmn

--- plugin/statuscolumn.lua
+++ plugin/statuscolumn.lua
@@ -6,37 +6,42 @@ function M.get_signs()
   local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
   return vim.tbl_map(function(sign)
     return vim.fn.sign_getdefined(sign.name)[1]
-  end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
+  end, vim.fn.sign_getplaced(buf, { group = '*', lnum = vim.v.lnum })[1].signs)
 end
 
 function M.column()
   local sign, git_sign
   for _, s in ipairs(M.get_signs()) do
-    if s.name:find("GitSign") then
+    if s.name:find('GitSign') then
       git_sign = s
     else
       if sign == nil then
         sign = s
+      else
+        goto continue
       end
     end
+    ::continue::
   end
 
-  local nu = " "
-  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, "number")
+  local nu = ' '
+  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, 'number')
   if number and vim.v.virtnum == 0 then
-    -- nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
     nu = vim.v.lnum
   end
+  -- if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
+  --   nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+  -- end
   local components = {
-    sign and ("%#" .. sign.texthl .. "#" .. sign.text .. "%*") or " ",
+    sign and ('%#' .. sign.texthl .. '#' .. sign.text .. '%*') or ' ',
     [[%=]],
-    nu .. "",
-    git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
+    nu .. '',
+    git_sign and ('%#' .. git_sign.texthl .. '#' .. git_sign.text .. '%*') or '  ',
   }
-  return table.concat(components, "")
+  return table.concat(components, '')
 end
 
-if vim.fn.has("nvim-0.9.0") == 1 then
+if vim.fn.has('nvim-0.9.0') == 1 then
   vim.opt.statuscolumn = [[%!v:lua.Status.column()]]
 end
 

d1912384669d960a9416f548d6893c9bd129968d
aphasia51 <xiahuas@outlook.com>
2023-03-26 13:03:56 +0800

    update

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -60,23 +60,21 @@ function config.toggleterm()
 end
 
 function config.colorizer()
-  vim.defer_fn(function()
-    local status_ok, colorizer = pcall(require, 'colorizer')
-    if not status_ok then
-      return
-    end
-    colorizer.setup({ '*' }, {
-      RGB = true,
-      RRGGBB = true,
-      names = false,
-      RRGGBBAA = true,
-      rgb_fn = true,
-      hsl_fn = true,
-      css = false,
-      css_fn = false,
-      mode = 'background',
-    })
-  end, 500)
+  local status_ok, colorizer = pcall(require, 'colorizer')
+  if not status_ok then
+    return
+  end
+  colorizer.setup({ '*' }, {
+    RGB = true,
+    RRGGBB = true,
+    names = false,
+    RRGGBBAA = true,
+    rgb_fn = true,
+    hsl_fn = true,
+    css = false,
+    css_fn = false,
+    mode = 'background',
+  })
 end
 
 function config.diffview()

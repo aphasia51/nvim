a26719bfa01a0392c8d8166515b90453ded26a83
aphasia51 <xiahuas@outlook.com>
2023-06-21 21:05:03 +0800

    nvim version check

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 vim.loader.enable()
 
-local NVIM_VERSION = require("utils").get_nvim_version()
-if NVIM_VERSION ~= "0.10.0" then
-    vim.api.nvim_err_writeln("Your nvim version is unexpected")
+local NVIM_VERSION = require('utils').get_nvim_version()
+if NVIM_VERSION ~= '0.10.0' then
+  vim.api.nvim_err_writeln('Your nvim version is unexpected')
 else
   require('config.lazy')
   require('config.options')
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,7 +2,15 @@ local M = {}
 
 function M.get_nvim_version()
   local version = vim.version()
-  return string.format("%d.%d.%d", version.major, version.minor, version.patch)
+  return string.format('%d.%d.%d', version.major, version.minor, version.patch)
+end
+
+_G.exec_filetype = function(group)
+  group = type(group) == 'string' and { group } or group
+  local curbuf = vim.api.nvim_get_current_buf()
+  for _, g in ipairs(group) do
+    vim.api.nvim_exec_autocmds('FileType', { group = g, pattern = vim.bo[curbuf].filetype })
+  end
 end
 
 return M

50e71da903b55864bfb0666c40daab1bfb79d2f5
aphasia51 <xiahuas@outlook.com>
2023-07-14 22:32:35 +0800

    update

--- init.lua
+++ init.lua
@@ -1,11 +1,6 @@
 vim.loader.enable()
 
-local NVIM_VERSION = require('utils').get_nvim_version()
-if NVIM_VERSION ~= '0.10.0' then
-  vim.api.nvim_err_writeln('Your nvim version is unexpected')
-else
-  require('config.lazy')
-  require('config.options')
-  require('config.autocmds')
-  require('config.keymaps')
-end
+require('config.lazy')
+require('config.options')
+require('config.autocmds')
+require('config.keymaps')

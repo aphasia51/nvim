74ff421db171b75664dc4dfe68104298b7d5d101
aphasia51 <xiahuas@outlook.com>
2023-08-07 20:40:32 +0800

    update

--- lua/utils/fmt.lua
+++ lua/utils/fmt.lua
@@ -1,10 +1,5 @@
 local M = {}
 
-function M.get_nvim_version()
-  local version = vim.version()
-  return string.format('%d.%d.%d', version.major, version.minor, version.patch)
-end
-
 function M.format_files()
   local ft = vim.bo.filetype
   if ft == 'python' then
